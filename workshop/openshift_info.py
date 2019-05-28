05caeace4c5a95a1e937a420f03f80ab042726b2
Anthony Byrne <abyrne@redhat.com>
2019-05-28 12:16:07 -0400

    Indentation fix

--- workshop/openshift_info.py
+++ workshop/openshift_info.py
@@ -25,7 +25,8 @@ class OpenShiftWorkshop(object):
         return self._get_running_pods(pod_list)
 
     def get_services(self):
-	services_api = self.oapi_client.resources.get(kind='Service', api_version='v1')
+        services_api = self.oapi_client.resources.get(kind='Service',
+                                                      api_version='v1')
         services_list = services_api.get(namespace=self.namespace)
         return self._get_names(services_list)
 
@@ -48,3 +49,4 @@ class OpenShiftWorkshop(object):
         return [pod.metadata.name
                 for pod in pods.items
                 if pod.status.phase == "Running"]
+
